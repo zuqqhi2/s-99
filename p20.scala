5a615f97ef69ba0ac2e61489a9a42d4eca6d2d02
Hidetomo Suzuki <hidetomo.suzuki@rakuten.com>
2016-11-09 18:02:00 +0900

    p20

--- p20.scala
+++ p20.scala
@@ -1,13 +1,32 @@
 def removeAt[A](removeIdx: Int, inputAry: List[A]): (List[A], A) = {
-  ( inputAry filter (_ != inputAry(removeIdx)), inputAry(removeIdx) )
+  if (removeIdx < 0 || inputAry.size == 0)
+    throw new NoSuchElementException
+  else
+    ( (inputAry.zipWithIndex filter (_._2 != removeIdx)) map (_._1), inputAry(removeIdx) )
 }
 
 /*
 scala> def removeAt[A](removeIdx: Int, inputAry: List[A]): (List[A], A) = {
-     |   ( inputAry filter (_ != inputAry(removeIdx)), inputAry(removeIdx) )
+     |   if (removeIdx < 0 || inputAry.size == 0)
+     |     throw new NoSuchElementException
+     |   else
+     |     ( (inputAry.zipWithIndex filter (_._2 != removeIdx)) map (_._1), inputAry(removeIdx) )
      | }
 removeAt: [A](removeIdx: Int, inputAry: List[A])(List[A], A)
 
 scala> removeAt(1, List('a, 'b, 'c, 'd))
 res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+
+scala> removeAt(1, List('a, 'b, 'b, 'c, 'd))
+res1: (List[Symbol], Symbol) = (List('a, 'b, 'c, 'd),'b)
+
+scala> removeAt(-1, List('a, 'b, 'b, 'c, 'd))
+java.util.NoSuchElementException
+  at .removeAt(<console>:13)
+  ... 32 elided
+
+scala> removeAt(10, List())
+java.util.NoSuchElementException
+  at .removeAt(<console>:13)
+  ... 32 elided
 */
